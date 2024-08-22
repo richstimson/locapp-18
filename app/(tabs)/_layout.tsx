a2c8ea7ca8d2fbae730e6998b80e514d54007dc6
richstimson <richstimson@yahoo.com>
2024-08-22 23:44:03 +0100

    Removed extraneous index tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,17 +5,20 @@ import { UserConfigProvider } from '../src/UserConfigContext';
 const TabsLayout = () => {
     return (
         <UserConfigProvider>
-        <Tabs>
-           <Tabs.Screen name="userConfigScreen" options={{
-                headerTitle: "User Config",
-            }}/>
-            <Tabs.Screen name="App" options={{
-                headerTitle: "Map",
-            }}/>
-        </Tabs>
+            <Tabs>
+            <Tabs.Screen name="userConfigScreen" options={{
+                    headerTitle: "User Config",
+                }}/>
+                <Tabs.Screen name="App" options={{
+                    headerTitle: "Map",
+                }}/>
+                <Tabs.Screen name="index" options={{
+                    tabBarButton: () => null, // Hide the index tab
+                }}/>
+            </Tabs>
         </UserConfigProvider>
     );
 };
 
-// Remove the export statement
+// Remove the export statementr
 export default TabsLayout;
